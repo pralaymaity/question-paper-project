97582bf2a431673c059451e7065b2f1ceaa01e68
prlaymaity <pralaymaity283@gmail.com>
2025-06-15 13:54:34 +0530

    .env db

--- backend/src/sequelize.js
+++ backend/src/sequelize.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize('question-paper', 'pralay', 1234, {
-  host: 'postgres',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
 
 module.exports = sequelize;
 
