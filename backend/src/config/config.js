fe4de4ac3962b98c3529956e593b8d1149355d54
prlaymaity <pralaymaity283@gmail.com>
2025-08-27 00:33:38 +0530

    render application

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,21 +1,31 @@
 require("dotenv").config();
-
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: "dpg-d2mqbcruibrs73bnp14g-a",
+let sequelize;
+
+if (process.env.DATABASE_URL) {
+  // On Render (or any hosted environment)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    protocol: "postgres",
     dialectOptions: {
       ssl: {
-        require: true, // required for Render Postgres
+        require: true,
         rejectUnauthorized: false,
       },
     },
-  }
-);
+  });
+} else {
+  // Local Docker setup
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+    }
+  );
+}
 
 module.exports = sequelize;
